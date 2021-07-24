bb3107a02512e82ddd627cf0009cdfd3288dba07
benrickken <benrickken@gmail.com>
2021-07-24 16:51:53 +0900

    Enable file upload

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var multer = require('multer')
+var upload = multer({ dest: './public/data/uploads/' })
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { mimetype: null, originalname: null, size: null })
+})
 
-module.exports = router;
+router.post('/', upload.single('uploaded-file'), function (req, res, next) {
+  const { file } = req
+  const { mimetype, originalname, size } = file
+  res.render('index', { mimetype, originalname, size })
+})
+
+module.exports = router
